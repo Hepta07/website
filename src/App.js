b8fe7031a26a0776106f3edc189809de50661687
Hamza-Ziyard <hamzaziyard.ux@gmail.com>
2024-01-03 15:58:21 +0530

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import "./App.css";
 import Home from "./pages/Home";
 
-import {HashRouter, Route, Routes, BrowserRouter} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import About from "./pages/About";
 import Schedule from "./pages/Schedule";
 import Page_404 from "./pages/404";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/schedule-a-call" element={<Schedule />} />
         <Route path="/*" element={<Page_404 />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
